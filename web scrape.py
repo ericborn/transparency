79a5ace6ec0d4d330a2dd82cda3ba3b35e61a11b
Eric Born <eric.born85@gmail.com>
2019-11-13 15:27:23 -0800

    Update web scrape.py
    Implemented beautiful soup to parse the page

--- web scrape.py
+++ web scrape.py
@@ -4,18 +4,20 @@ Created on Wed Nov 13 13:37:33 2019
 
 @author: Eric Born
 """
-import json
-import requests
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup as bs
 
+# setup options to make chrome headless
 opts = Options()
-opts.set_headless()
+opts.headless = True
 
 # loads the chromedriver exe which opens a chrome window
-driver = webdriver.Chrome(r'C:/chomedriver/chromedriver.exe')
+driver = webdriver.Chrome(r'C:/chomedriver/chromedriver.exe', options=opts)
+
+# comment out three lines above if chrome needs to be run with a head
+#driver = webdriver.Chrome(r'C:/chomedriver/chromedriver.exe')
 
 # loads the URL
 driver.get('https://c6axa542.caspio.com/dp/b5be7000b00c6c32872649618e72?rnd=1573681939421')
@@ -24,29 +26,12 @@ driver.get('https://c6axa542.caspio.com/dp/b5be7000b00c6c32872649618e72?rnd=1573
 botton = driver.find_element_by_name('searchID')
 botton.submit()
 
-
-
-# Pulls down the main page
-#response = requests.get('https://www.10news.com/transparency-project-database-search-officer-involved-shootings-law-enforcement-misconduct-cases')
-response = requests.get('https://c6axa542.caspio.com/dp/b5be7000b00c6c32872649618e72?rnd=1573681939421')
-
-
-json_response = ((response.text.split))
-
-
-
-# Decodes the page
-page_src = response.content.decode('utf-8')
-
 # convert the page to beautiful soup format
-soup = bs(page_src, 'html.parser')
+soup = bs(driver.page_source, 'html.parser')
 
+# selects all table data
+table = soup.findAll('div', {"class": "cbResultSetPanelDataContainer"})
 
-
-# Creates an empty list to store all weapon main view page links
-weapLinks = []
-
-# Store links in a list
-for link in soup.find_all('a'):
-    #print(link.get('href'))
-    weapLinks.append(link.get('href'))
+# prints text between the span elements
+for span in soup.select('span'):
+    print(span.text)
